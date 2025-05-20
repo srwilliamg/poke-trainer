8434352f6613f1d94e001090ba5d85f0b7aee266
William Salazar <william@Williams-MacBook-Pro.local>
2025-05-20 17:49:25 -0500

    feat: add button behavior

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,9 @@
 import './App.css';
 
+import { ChangeEvent, useState } from 'react';
+
 import { Button } from '@/components/ui/button';
+import { FaArrowRight } from "react-icons/fa6";
 import { Input } from '@/components/ui/input';
 
 // const users = [
@@ -35,20 +38,33 @@ import { Input } from '@/components/ui/input';
 // ];
 
 function App() {
+  const [ inputVal, setInputVal] = useState("")
+  
+  function nextClick(event: React.MouseEvent<HTMLButtonElement>) {
+    console.log(event, inputVal)
+  }
+  
+  function onChangeInput(event: ChangeEvent<HTMLInputElement>) {
+    if (event.target.value) {
+      setInputVal(event.target.value)
+    }
+  }
+  
   return (
     <>
-      <div className="flex flex-col gap-2 h-screen bg-poke-umbreon">
-        <div className="flex flex-col w-full h-8/12 items-center justify-around">
+      <div className="flex flex-col gap-2 h-screen bg-amber-50">
+        <div className="flex flex-col w-full h-8/12 items-center justify-center">
           <div className="flex flex-col justify-around">
             <div className='className="flex w-full'>
-              <h1 className="text-center text-poke-yellow">
+              <h1 className="text-center text-poke-rapidash font-medium font-stretch-95%">
                 What's your pokemon trainer name?
               </h1>
             </div>
-            <div className='className="flex w-full'><br /></div>
-            <div className="flex w-full items-center space-x-2 justify-around">
-              <Input className='border-none hover:border-none focus:border-none focus:' type="text" placeholder="Trainer"/>
-              <Button className='bg-poke-blue hover:bg-poke-squirrel' type="submit">Next</Button>
+            <div className="flex w-8/12 items-center space-x-2 self-center my-3">
+              <Input onChange={onChangeInput} className='border-l-0 border-r-0 border-t-0 shadow-none outline-none not-focus:animate-bounce not-focus:duration-1000' type="text" />
+            </div>
+            <div className="flex w-full items-center space-x-2 self-center justify-around">
+              <Button className='bg-transparent shadow-none focus:ring-0 text-gray-500 hover:bg-poke-rapidash hover:scale-120 cursor-pointer' type="submit" onClick={nextClick}>Next <FaArrowRight /></Button> 
             </div>
           </div>
         </div>
